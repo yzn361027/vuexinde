c7d8ca7a50c523d21ffe69bd2c30c5cc702b8a64
yzn361027 <869016838@qq.com>
2020-10-09 11:54:40 +0800

    我的第一次提交

--- src/network/request.js
+++ src/network/request.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 
 export function request(config){
   const instance=axios.create({
-    baseURL:"http://123.207.32.32:8000/",
+    baseURL:"http://kfbltz:8080",
     timeout:5000,
     method:"get",
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    },
+    // headers: {
+    //   'Content-Type': 'application/json;charset=UTF-8'
+    // },
   });
   // 发送请求拦截 可以添加请求成功或者失败的
   instance.interceptors.request.use(config=>{
@@ -19,7 +19,7 @@ err=>{
     console.log(2222)
   }
 );
-  //相应拦截
+  //响应拦截
   instance.interceptors.response.use(res =>{
    return res
 
@@ -31,21 +31,13 @@ err=>{
   )
 return instance(config);
 }
-// export function request(config){
-//   return new Promise((resolve,reject) => {
-//     const instance = axios.create({
-//       baseURL: 'http://123.207.32.32:8000',
-//       timeout: 5000
-//     });
-//   return instance(config)
-// }
 
 export function request2(config){
   return new Promise((resolve,reject) => {
     const instance=axios.create({
-      baseURL:"http://192.168.0.198:8080/wemall/wap/cg/",
+      baseURL:"http://192.168.0.22:8080",
       timeout:5000,
-      method:"post",
+      method:"get",
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
       },
@@ -54,5 +46,18 @@ export function request2(config){
       resolve(res)
     })
   })
+}
+
+export function xiangqing(config){
+  const instance=axios.create({
+    baseURL:"http://192.168.0.22:8080/wap/cg/",
+    timeout:5000,
+    method:'get',
+    headers:{
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    params: {}
+  })
+
 }
 
